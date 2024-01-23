ff1bda2031e03d53bac45fe626dc0ba0a1e2e298
km1031kim <km1031kim@naver.com>
2024-01-23 20:57:01 +0900

    till 4.1

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -1,1 +1,39 @@
-class Sizes {}
+// 디자이너가 없으면..UI 구현이 힘들다.
+// margin이나 padding 을 줄 때 어떤 값을 줘야 하는지.
+// 구체적인 size표나 시스템이 없다면 UI 구현이 짜증남.
+// 여기저기서 마진을 다르게 주고, 여백을 다르게 주니까.
+// 다 완성했을 때 디자인이 일관성이 없음. 사이징 시스템이 없으니까
+// tailwindcss css framework에서 sizing 수치 복사 후 dart 코드로 변환
+// 그 코드를 아래 코드에 복붙.
+
+class Sizes {
+  static const size1 = 1.0;
+  static const size2 = 2.0;
+  static const size3 = 3.0;
+  static const size4 = 4.0;
+  static const size5 = 5.0;
+  static const size6 = 6.0;
+  static const size7 = 7.0;
+  static const size8 = 8.0;
+  static const size9 = 9.0;
+  static const size10 = 10.0;
+  static const size11 = 11.0;
+  static const size12 = 12.0;
+  static const size14 = 14.0;
+  static const size16 = 16.0;
+  static const size20 = 20.0;
+  static const size24 = 24.0;
+  static const size28 = 28.0;
+  static const size32 = 32.0;
+  static const size36 = 36.0;
+  static const size40 = 40.0;
+  static const size44 = 44.0;
+  static const size48 = 48.0;
+  static const size52 = 52.0;
+  static const size56 = 56.0;
+  static const size60 = 60.0;
+  static const size64 = 64.0;
+  static const size72 = 72.0;
+  static const size80 = 80.0;
+  static const size96 = 96.0;
+}
---
+++ lib/features/authentication/login_screen.dart
@@ -0,0 +1,77 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  void onSignUpTap(BuildContext context) {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: Sizes.size40,
+          ),
+          child: Column(
+            children: [
+              Gaps.v80,
+              Text(
+                "Log in to TikTok",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: Sizes.size24,
+                    fontWeight: FontWeight.w700),
+              ),
+              Gaps.v20,
+              Text(
+                "Create a profile, follow other accounts, make your own videos, and more.",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  color: Colors.black45,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.grey.shade100,
+        elevation: 2,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: Sizes.size5,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Don't have an account?",
+              ),
+              Gaps.h5,
+              GestureDetector(
+                onTap: () => onSignUpTap(context),
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size16),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
