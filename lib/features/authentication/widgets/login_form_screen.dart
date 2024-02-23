9ef8f38a1ad4f574d5d80dfe235438ba1cc50d5e
km1031kim <km1031kim@naver.com>
2024-02-23 20:47:08 +0900

    form button challenge

--- lib/features/authentication/widgets/login_form_screen.dart
+++ lib/features/authentication/widgets/login_form_screen.dart
@@ -1,10 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Map<String, String> formData = {};
+
+  void _onSubmitTap() {
+    if (_formKey.currentContext != null) {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+        //print(formData.entries);
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('log in')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Gaps.h28,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+                validator: (value) {
+                  // value는 TextFormField의 값임.
+                  // 에러 메세지를 반환하거나 안 할 수 있음
+                  return null;
+                },
+                onSaved: (newValue) => {
+                  if (newValue != null) {formData['email'] = newValue}
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                validator: (value) {
+                  // value는 TextFormField의 값임.
+                  // 에러 메세지를 반환하거나 안 할 수 있음
+                  return null;
+                },
+                onSaved: (newValue) => {
+                  if (newValue != null) {formData['password'] = newValue}
+                },
+              ),
+              Gaps.v16,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: const FormButton(
+                  disabled: false,
+                  text: "Log in",
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/authentication/widgets/login_screen.dart
+++ lib/features/authentication/widgets/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
+import 'package:tiktok_clone/features/authentication/widgets/login_form_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -9,27 +12,32 @@ class LoginScreen extends StatelessWidget {
     Navigator.of(context).pop();
   }
 
-  test(BuildContext context, Widget target) {
-    return Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => target,
-      ),
-    );
+  // test(BuildContext context, Widget target) {
+  //   return Navigator.of(context).push(
+  //     MaterialPageRoute(
+  //       builder: (context) => target,
+  //     ),
+  //   );
+  // }
+
+  void _onEmailLoginTap(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => const LoginFormScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: const SafeArea(
+      body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(
+          padding: const EdgeInsets.symmetric(
             horizontal: Sizes.size40,
           ),
           child: Column(
             children: [
               Gaps.v80,
-              Text(
+              const Text(
                 "Log in to TikTok",
                 style: TextStyle(
                     color: Colors.black,
@@ -37,7 +45,7 @@ class LoginScreen extends StatelessWidget {
                     fontWeight: FontWeight.w700),
               ),
               Gaps.v20,
-              Text(
+              const Text(
                 "Create a profile, follow other accounts, make your own videos, and more.",
                 style: TextStyle(
                   fontSize: Sizes.size16,
@@ -46,15 +54,18 @@ class LoginScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Gaps.v40,
-              //AuthButton(
-              //  icon: FaIcon(FontAwesomeIcons.user),
-              // text: "Use email & password",
-              // ),
+              GestureDetector(
+                onTap: () => _onEmailLoginTap(context),
+                child: const AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.user),
+                  text: "Use email & password",
+                ),
+              ),
               Gaps.v16,
-              //AuthButton(
-              //  icon: FaIcon(FontAwesomeIcons.apple),
-              // text: "Continue with Apple",
-              //),
+              const AuthButton(
+                icon: FaIcon(FontAwesomeIcons.apple),
+                text: "Continue with Apple",
+              ),
             ],
           ),
         ),
